f4292da731458019ac7b2761b702532903e4498b
dybq1 <dybq1234@gmail.com>
2025-12-22 12:47:04 -0500

    ok

--- src/estudiante.cpp
+++ src/estudiante.cpp
@@ -11,8 +11,6 @@ Estudiante :: Estudiante()
 	: Persona(), id(count)
 { 
 	count++; 
-	notas[0] = 0;
-	notas[1] = 0;
 	cursos = nullptr;
 }
 
@@ -21,8 +19,6 @@ Estudiante :: Estudiante(const string& nombre)
 	: Persona(nombre), id(count)
 { 
 	count++; 
-	notas[0] = 0;
-	notas[1] = 0;
 	cursos = nullptr;
 }
 	
@@ -31,8 +27,6 @@ Estudiante :: Estudiante (const Estudiante &e)
 	: Persona(e), id(e.id) 
 {
 	count++;
-	notas[0] = e.notas[0];
-	notas[1] = e.notas[1];
 	cursos = nullptr;
 }
 
@@ -43,21 +37,13 @@ void Estudiante :: mostrarInfo() const {
 
 //calcular promedio		
 float Estudiante :: calcularDesempeno() const {
-	return ( notas[0] + notas[1] )/2.0f;
+	//rellenar
+	return 0.0f;
 }
 
 //agregar notas		
 void Estudiante :: agregarNota(){
-	int op;
-	float no;
-	cout << "Desea agregar nota del parcial 1 o 2? "; cin >> op;
-	cout << "Nota: "; cin >> no;
-	
-	if (op == 1){
-		notas[0] = no;
-	} else {
-		notas[1] = no;
-	}
+	//rellenar
 		
 };
 
