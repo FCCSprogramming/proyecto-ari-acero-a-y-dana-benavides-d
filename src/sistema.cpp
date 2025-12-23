1b4f06cc74cd03278833e5021b24f6ab8b1fbfc2
ari1639 <arialva322@gmail.com>
2025-12-23 14:36:23 -0500

    vacio

---
+++ src/sistema.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "../include/curso.h"
+
+// constructor
+Curso ::Curso()
+{
+    this->nombre = "";
+    this->id = "00";
+    this->c_creditos = 0;
+}
+Curso ::Curso(string nombre, string id, int c_creditos)
+{
+    this->nombre = nombre;
+    this->id = id;
+    this->c_creditos = c_creditos;
+}
+void Curso ::show_inf_cur() const
+{
+    cout << "ID: " << this->id << " | Nombre del curso: " << this->nombre;
+}
